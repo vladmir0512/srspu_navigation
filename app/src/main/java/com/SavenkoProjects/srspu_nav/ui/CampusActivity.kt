59a175b9bec90bb856d6b2c7537ab51c22a401a4
vj <vjgamer2001@gmail.com>
2025-05-21 15:38:29 +0300

    highlight building on campus map

--- app/src/main/java/com/SavenkoProjects/srspu_nav/ui/CampusActivity.kt
+++ app/src/main/java/com/SavenkoProjects/srspu_nav/ui/CampusActivity.kt
@@ -4,10 +4,12 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.webkit.JavascriptInterface
+import android.webkit.WebView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.SavenkoProjects.srspu_nav.databinding.ActivityCampusBinding
 import com.SavenkoProjects.srspu_nav.data.Constants.BUILDING_ID
+import com.SavenkoProjects.srspu_nav.utils.IntentConstants.EXTRA_BUILDING_ID
 
 class CampusActivity : AppCompatActivity() {
 
@@ -30,15 +32,31 @@ class CampusActivity : AppCompatActivity() {
 		super.onCreate(savedInstanceState)
 		val binding = ActivityCampusBinding.inflate(layoutInflater)
 		setContentView(binding.root)
-		setWebView(binding)
+		
+		// Получаем buildingId из Intent
+		val buildingId = intent.getStringExtra(EXTRA_BUILDING_ID) ?: "6"
+		
+		setWebView(binding, buildingId)
 	}
 
-	private fun setWebView(binding: ActivityCampusBinding) {
+	private fun setWebView(binding: ActivityCampusBinding, buildingId: String) {
 		val webView = binding.webView
 		webView.settings.javaScriptEnabled = true
 		webView.addJavascriptInterface(WebAppInterface(this), "Android")
 		webView.loadUrl("file:///android_asset/map.html")
 		webView.settings.builtInZoomControls = true
 		webView.settings.displayZoomControls = true
+		
+		// Добавляем слушатель загрузки страницы
+		webView.webViewClient = object : android.webkit.WebViewClient() {
+			override fun onPageFinished(view: WebView?, url: String?) {
+				super.onPageFinished(view, url)
+				// Вызываем JavaScript функцию для подсветки корпуса
+				webView.evaluateJavascript(
+					"javascript:highlightBuilding('$buildingId')",
+					null
+				)
+			}
+		}
 	}
 }
